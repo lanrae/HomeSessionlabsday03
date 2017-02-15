5cc0777db5cdc93653c7d75ff0695027e3476105
Fawaz Bello <info@lanraeictsolutions.com>
2017-02-15 17:26:34 +0100

    Test suites Added

---
+++ Missing-Number/spec/findMissing-spec.js
@@ -0,0 +1,67 @@
+var findMissing = require('../app/findMissing.js')
+
+describe("Find missing number two lists: ", function() {
+
+  describe("Case for missing number in each of the arrays", function() {
+
+    it("should return [1, 3] for [1, 2, 5], [2, 5, 3]", function() {
+      expect(findMissing([1, 2, 5], [2, 5, 3])).toEqual([1, 3]);
+    });
+
+    it("should return [4, 9] for [5, 7, 4], [5, 7, 9]", function() {
+      expect(findMissing([5, 7, 4], [5, 9, 7])).toEqual([4, 9]);
+    });
+  });
+
+  describe("Case for missing number in first array but none in second array", function() {
+
+    it("should return 0 for [1, 2, 5], [2, 5]", function() {
+      expect(findMissing([1, 2, 5], [2, 5])).toEqual(0);
+    });
+
+    it("should return 0 for [5, 7, 4], [5, 7]", function() {
+      expect(findMissing([5, 7, 4], [5, 7])).toEqual(0);
+    });
+  });
+
+  describe("Case for en empty list", function() {
+
+    it("should return 0 for empty list", function() {
+      expect(findMissing([], [])).toEqual(0);
+    });
+
+  });
+
+  describe("Case for lists with the same entries", function() {
+
+    it("should return 0 for [2],[2]", function() {
+      expect(findMissing([2], [2])).toEqual(0);
+    });
+
+    it("should return 0 for [4],[4]", function() {
+      expect(findMissing([4], [4])).toEqual(0);
+    });
+
+    it("should return 0 for [7],[7]", function() {
+      expect(findMissing([7], [7])).toEqual(0);
+    });
+
+  });
+
+  describe("Case for lists with similar entries and a missing number", function() {
+
+    it("should return 5 for [1,2],[1,2,5]", function() {
+      expect(findMissing([1, 2, 3], [1, 2])).toEqual(0);
+    });
+
+    it("should return 10 for [4, 6, 8],[4, 6, 8, 10]", function() {
+      expect(findMissing([4, 6, 8], [4, 6, 8, 10])).toEqual(10);
+    });
+
+    it("should return 1 for [5, 4, 7, 6, 11, 66],[5, 4, 1, 7, 6, 11, 66]", function() {
+      expect(findMissing([5, 4, 7, 6, 11, 66], [5, 4, 1, 7, 6, 11, 66])).toEqual(1);
+    });
+
+  });
+
+});
